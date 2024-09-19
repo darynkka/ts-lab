8fb807578319813f9cde6010898b69fcaa35550e
darynkka <dashalitviniuk20@gmail.com>
2024-09-19 23:18:28 +0300

    vitalik дякую

--- test/validation.test.ts
+++ test/validation.test.ts
@@ -1,13 +1,13 @@
-import { expect } from 'chai';
-import { Validation } from '../src/validation';
+import { strict as assert } from 'assert';
+import { Validation } from '../src/validation'; // Оновіть шлях до файлу, якщо потрібно
 
 describe('Validation Class', () => {
   it('should validate book correctly', () => {
     const valid = Validation.validateBook('Title', 'Author', '2024');
-    expect(valid).to.deep.equal({});
+    assert.deepEqual(valid, {});
 
     const invalid = Validation.validateBook('', '', 'invalid');
-    expect(invalid).to.deep.equal({
+    assert.deepEqual(invalid, {
       bookTitle: 'Назва книги не може бути пустою',
       bookAuthor: 'Автор не може бути пустим',
       bookYear: 'Рік видання повинен бути числом',
@@ -16,10 +16,10 @@ describe('Validation Class', () => {
 
   it('should validate user correctly', () => {
     const valid = Validation.validateUser('Name', 'email@example.com');
-    expect(valid).to.deep.equal({});
+    assert.deepEqual(valid, {});
 
     const invalid = Validation.validateUser('', 'invalidemail');
-    expect(invalid).to.deep.equal({
+    assert.deepEqual(invalid, {
       userName: "Ім'я не може бути пустим",
       userEmail: 'Email не є дійсним',
     });
